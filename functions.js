32f0ae8267e1b560240bb876501411abd4425467
DanFogelberg <dan.f.fogelberg@gmail.com>
2022-12-10 14:41:34 +0100

    Did too much stuff...
    Added css-variables, font, fixed fire spreading mechanics.
    
    Fire spreads sometimes only,  and in a random direction. Now doesn't crash at edges or set fire to rivers.

--- functions.js
+++ functions.js
@@ -1,37 +1,93 @@
 //Switches type of tile to new type
-const changeType = (tile) => {
+const changeType = (tile, newTile = false) => {
   console.log(tile);
   removeTypes(tile);
-  tile.classList.add(nextTile);
-  tile.dataset.type = nextTile;
+  // tile.dataset.cooldown = true; //Potenial problem if happens on tiles that haven't actually changed type
+  console.log(newTile);
 
-  arrayIndex = Math.floor(Math.random() * tileTypes.length);
-  nextTile = tileTypes[arrayIndex];
-
-  removeTypes(infoTile);
-  infoTile.classList.add(nextTile);
-}
-
+  //If function is called without a specific tile, gets tile from stack
+  if (newTile === false) {
+    tile.classList.add(nextTile);
+    tile.dataset.type = nextTile;
+    arrayIndex = Math.floor(Math.random() * tileTypes.length);
+    nextTile = tileTypes[arrayIndex];
+    removeTypes(infoTile);
+    infoTile.classList.add(nextTile);
+  } else {
+    tile.classList.add(newTile);
+    tile.dataset.type = newTile;
+  }
+};
 
 //Unset tile
 const removeTypes = (tile) => {
-  tileTypes.forEach(tileType => {
+  tileTypes.forEach((tileType) => {
     tile.classList.remove(tileType);
   });
-}
+};
 
-
+//Activate all tiles. Recursive.
 const tick = () => {
-
-  map.forEach(row=>{
-
-    row.forEach(tile => {
-      console.log(tile);
-    })
-
-  })
-
-
-
+  map.forEach((row) => {
+    row.forEach((tile) => {
+      console.log(tile.dataset.cooldown);
+      if (tile.dataset.cooldown == 'true') {
+        tile.dataset.cooldown = false;
+      } else {
+        activate(tile);
+      }
+    });
+  });
   setTimeout(tick, 5000);
-}
+};
+
+const activate = (tile) => {
+  switch (tile.dataset.type) {
+    case 'fire': //Fire spreads to surrounding squares. Blocked by water.
+      if (Math.random() > 0.2) break; //20% chance of fire spreading
+
+      const direction = Math.random();
+      let targetTile;
+      if (direction < 0.25) {
+        console.log('right');
+        if (parseInt(tile.dataset.x) + 1 > mapWidth - 1) break;
+        targetTile =
+          map[parseInt(tile.dataset.y)][1 + parseInt(tile.dataset.x)]; //Right
+      } else if (direction < 0.5) {
+        console.log('down');
+        if (parseInt(tile.dataset.y) + 1 > mapHeight - 1) break;
+        targetTile =
+          map[1 + parseInt(tile.dataset.y)][parseInt(tile.dataset.x)]; //Down
+      } else if (direction < 0.75) {
+        console.log('left');
+        if (parseInt(tile.dataset.x) - 1 < 0) break;
+        targetTile =
+          map[parseInt(tile.dataset.y)][parseInt(tile.dataset.x) - 1]; //Left
+      } else {
+        console.log('up');
+        if (parseInt(tile.dataset.y) - 1 < 0) break;
+        targetTile =
+          map[parseInt(tile.dataset.y) - 1][parseInt(tile.dataset.x)]; //Up
+      }
+      spreadFire(targetTile);
+      console.log(map[parseInt(tile.dataset.y)][1 + parseInt(tile.dataset.x)]);
+
+      break;
+    case 'forest':
+      //console.log("Skoga");
+      //Växer till större skog och ibland sprids det småskog
+      break;
+
+    case 'river':
+    //console.log("Det rinner långsamt.");
+    //Kanske rinner floden ibland? Oklart.
+    default:
+    // code block
+  }
+};
+
+const spreadFire = (tile) => {
+  if (tile.dataset.type != 'river') {
+    changeType(tile, 'fire');
+  }
+};
--- script.js
+++ script.js
@@ -1,67 +1,52 @@
-const tileTypes = ["forest", "river", "fire"];
+const tileTypes = ['forest', 'river', 'fire'];
+const mapHeight = 10;
+const mapWidth = 20;
 
+const body = document.querySelector('body');
+const infoBar = document.createElement('section');
+infoBar.classList.add('info-bar');
 
-
-const body = document.querySelector("body");
-const infoBar = document.createElement("section");
-infoBar.classList.add("info-bar");
-
-
-
-const infoTile = document.createElement("div");
-infoTile.classList.add("info-tile");
-infoTile.classList.add("tile");
-infoTile.classList.add("forest");
-
-
-
-
-
-
+const infoTile = document.createElement('div');
+infoTile.classList.add('info-tile');
+infoTile.classList.add('tile');
+infoTile.classList.add('forest');
 
 let arrayIndex = Math.floor(Math.random() * tileTypes.length);
 let nextTile = tileTypes[arrayIndex];
 initialTile = tileTypes[0];
-console.log(nextTile)
+console.log(nextTile);
 
 //Create map
 let map = [];
-const mapSection = document.createElement('section')
-mapSection.classList.add("map-section");
+const mapSection = document.createElement('section');
+mapSection.classList.add('map-section');
 body.append(mapSection);
-for(let i = 0; i < 10; i++){
+for (let i = 0; i < mapHeight; i++) {
   map[i] = []; //Make row into an array of tiles
-  let row = document.createElement('row')
-  row.classList.add("row");
+  let row = document.createElement('row');
+  row.classList.add('row');
   mapSection.append(row);
 
   //Fill row with tiles
-  for(let j = 0; j < 20; j++){
+  for (let j = 0; j < mapWidth; j++) {
     map[i][j] = document.createElement('div');
-    map[i][j].classList.add("tile");
+    map[i][j].classList.add('tile');
     map[i][j].classList.add(initialTile);
-    map[i][j].addEventListener("click", changeType.bind(null, map[i][j]));
+    map[i][j].addEventListener(
+      'click',
+      changeType.bind(null, map[i][j], false)
+    );
 
     map[i][j].dataset.type = initialTile;
+    map[i][j].dataset.y = i;
+    map[i][j].dataset.x = j;
 
     row.append(map[i][j]);
   }
 }
 
-
 body.append(infoBar);
 infoBar.append(infoTile);
 
-
-
 //Every 5 seconds all the tiles might do something.
 tick();
-
-
-
-
-
-
-
-
-
